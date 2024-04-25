4931d8bc106aea5c7379274fec8fa56f1a922e98
David Trujillo <d.trujillo@unc.edu.ar>
2024-04-24 21:02:13 -0300

    Implemented ftoi.asm and main.c

--- ftoi.asm
+++ ftoi.asm
@@ -1,74 +1,17 @@
-; Carga el valor float en el acumulador (EAX)
-mov eax,flt
+section .data                   ;initialized data
+    ftoi dd 0                   ;dword initialized as 0
 
-; Desplazamiento a la izquierda de EAX para eliminar el signo
-rcl eax,1
+global asm_main                 ;declared as global to use ot outside of this file
+section .text                   ;code section
 
-; Guarda el valor de EAX en EBX para uso posterior
-mov ebx,eax
-
-; Limpia EDX
-mov edx,4278190080
-
-; Obtiene el exponente (bits 24-31 de EAX) y lo guarda en EAX
-and eax,edx
-shr eax,24
-sub eax,7fh
-
-; Guarda el exponente en EDX para uso posterior
-mov edx,eax
-
-; Elimina los 8 bits más a la izquierda de EAX (que contienen el exponente)
-mov eax,ebx
-rcl eax,8
-mov ebx,eax
-
-; Resta 17h (27) del exponente y guarda el resultado en ECX
-mov ecx, 1fh
-sub ecx,edx
-
-; Inicializa EDX a 0
-mov edx,00000000h
-
-; Compara ECX con 0 (si el exponente es negativo)
-cmp ecx,0
-
-; Si el exponente es 0, salta a la etiqueta "loop2"
-je loop2
-
-; Desplaza EAX a la derecha una vez y OR con 80000000h (si el exponente es positivo)
-loop1:
-    shr eax,1
-    or eax,80000000h
-    sub ecx,1
-    add edx,1
-    cmp ecx,0
-    ja loop1
-
-; Etiqueta para el final del bucle de desplazamiento
-loop2:
-
-; Mueve el valor final de EAX a la variable de retorno "i"
-mov i, eax
-
-; Etiqueta para determinar el signo (opcional)
-sign:
-
-; Obtiene el bit de signo del valor float
-mov eax,flt
-and eax,80000000h
-
-; Compara el bit de signo con 80000000h (signo negativo)
-cmp eax,80000000h
-
-; Si el signo es negativo, salta a la etiqueta "putsign"
-je putsign
-
-; Retorna el valor de "i" (positivo)
-ret
-
-; Etiqueta para retornar el valor negativo (opcional)
-putsign:
-
-; Retorna -i (negativo)
-ret
+ftoi:                           ;function
+    push ebp                    ;push base pointer to save it (equivalent to "enter" instruction)
+    mov ebp, esp                ;move stack pointer to base pointer
+    fld dword [esp + 8]         ;load float into numeric coprocessor stack
+    fistp dword[ftoi]           ;pops the numeric stack, converts to integer and stores in ftoi
+    mov eax, [ftoi]             ;move ftoi to general register eax
+    add eax, 1                  ;add 1 to eax
+    mov [ftoi], eax             ;move eax to ftoi, not needed cos i only need to return it and it's already in eax
+    mov esp, ebp                ;restores stack pointer
+    pop ebp                     ;restores base pointer
+    ret                         ;return from function
---
+++ main.c
@@ -0,0 +1,6 @@
+extern int ftoi(float);
+
+int cFloatToInt(float f_gini){
+    int i_gini = ftoi(f_gini);
+    return i_gini;
+}
